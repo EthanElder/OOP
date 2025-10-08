44d43979d2be2d0a8876e7bcca77f1de39ed07be
EthanElder <ElderEt@jbu.edu>
2025-10-08 09:48:29 -0500

    Add files via upload

---
+++ Classwork 11.1 HW.py
@@ -0,0 +1,7 @@
+#Define a class Book(BookID, BookName, Title, Author)
+#Define a class User(userID, userName, BooksBorrowed=[])
+
+#Create 5 book objects
+
+#Create 2 user objects
+#Add books to the users
